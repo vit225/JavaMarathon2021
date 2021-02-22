271ce3cfc6d20b23c588318c17231260110711fe
vit225 <vitaly1090@mail.ru>
2021-02-23 08:38:14 +1000

    complete day1 tasks

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -4,9 +4,9 @@ public class Task6 {
     public static void main(String[] args) {
         int k = 8;
         int i = 1;
-       while (i < 9){
-           System.out.println(i + " x " + k + " = " + i * k);
-           i++;
-       }
+        while (i <= 9) {
+            System.out.println(i + " x " + k + " = " + i * k);
+            i++;
+        }
     }
 }
