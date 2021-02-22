8359ff96651093cfbd2bd0d527f5325cb4cd9267
vit225 <vitaly1090@mail.ru>
2021-02-23 08:40:34 +1000

    complete day1 tasks

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,7 +2,7 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
-         int i = 0;
+        int i = 0;
         while (i < 10) {
             System.out.println("JAVA");
             i++;
