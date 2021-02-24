3fa05230235e1e52d0bce20bfa9f74e4da1e8698
vit225 <vitaly1090@mail.ru>
2021-02-24 22:48:58 +1000

    complete day3 tasks

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,22 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        double a;
+        double b;
+        int i = 0;
+        while (i < 5) {
+            ++i;
+            a = scanner.nextDouble();
+            b = scanner.nextDouble();
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(a / b);
+        }
     }
 }
