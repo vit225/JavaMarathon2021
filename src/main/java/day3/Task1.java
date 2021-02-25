bc02a9b69b9bae363580d949d9520faf74d27c51
vit225 <vitaly1090@mail.ru>
2021-02-25 12:10:05 +1000

    complete day3 tasks1

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -5,33 +5,32 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String country = scanner.nextLine();
-        switch (country) {
-            case "stop":
-                System.out.println("программа завершила работу");
-                break;
-            case "Москва":
-            case "Владивосток":
-            case "Ростов":
-                System.out.println("Россия");
-                break;
-            case "Рим":
-            case "Милан":
-            case "Турин":
-                System.out.println("Италия");
-                break;
-            case "Ливерпуль":
-            case "Манчестер":
-            case "Лондон":
-                System.out.println("Англия");
-                break;
-            case "Берлин":
-            case "Мюнхен":
-            case "Кёльн":
-                System.out.println("Германия");
-                break;
-            default:
-                System.out.println("Неизвестная страна");
+        String country;
+        while (!(country = scanner.nextLine()).equals("Stop")) {
+            switch (country) {
+                case "Москва":
+                case "Владивосток":
+                case "Ростов":
+                    System.out.println("Россия");
+                    break;
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                    System.out.println("Италия");
+                    break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                    System.out.println("Германия");
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+            }
         }
     }
 }
