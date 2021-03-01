5d5e02382d31a5af3b057cd3416d288bde36e170
vit225 <vitaly1090@mail.ru>
2021-03-01 20:36:03 +1000

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,22 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        String string = "";
+        long startString = System.nanoTime();
+        for (int i = 0; i <= 20000; i++) {
+            string = string + (i + " ");
+        }
+        long finishString = System.nanoTime();
+        System.out.println(string);
+        StringBuilder sb = new StringBuilder();
+        long startSb = System.nanoTime();
+        for (int i = 1; i <= 20000; i++) {
+            sb.append(i + " ");
+        }
+        long finishSb = System.nanoTime();
+        System.out.println("");
+        System.out.println(sb);
+        System.out.println(finishString - startString);
+        System.out.println(finishSb - startSb);
     }
 }
