c22d08467d48a8fdc45109c06ea0b0ae2de0abbf
vit225 <vitaly1090@mail.ru>
2021-03-10 19:12:41 +1000

    complete day14 tasks

--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,10 +1,36 @@
 package day14;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
         File file = new File("people");
-        System.out.println(Person.parseFileToObjList(file));
+        System.out.println(parseFileToObjList(file));
+    }
+
+    public static List<Person> parseFileToObjList(File file) {
+        List<Person> people = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String string = scanner.nextLine();
+                String[] human = string.split(" ");
+                if (Integer.parseInt(human[1]) < 0) {
+                    throw new IOException();
+                }
+                Person person = new Person(human[0], Integer.parseInt(human[1]));
+                people.add(person);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IOException e) {
+            System.out.println("Некорректный входной файл");
+        }
+        return people;
     }
 }
