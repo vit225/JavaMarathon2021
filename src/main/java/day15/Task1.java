706ce6e9fc1d173df51c2c3593d15dc03131fb21
vit225 <vitaly1090@mail.ru>
2021-03-10 20:37:01 +1000

    complete day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,31 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        String s = File.separator;
+        File file = new File("src" + s + "main" + s + "resources" + s + "shoes.csv");
+        try {
+            Scanner scanner = new Scanner(file);
+            PrintWriter pw = new PrintWriter("src" + s + "main" + s + "resources" + s + "missing_shoes.txt");
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] shoes = line.split(";");
+                if (Integer.parseInt(shoes[2]) == 0) {
+                    pw.print(shoes[0]);
+                    pw.print(", ");
+                    pw.print(shoes[1]);
+                    pw.print(", ");
+                    pw.println(shoes[2]);
+                }
+            }
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
